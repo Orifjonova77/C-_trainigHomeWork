a5b50114fbc6e14106cdf0e2166fc97526c576d5
Orasta <mirdadoeva.o@gmail.com>
2021-11-24 17:06:10 +0500

    Init

---
+++ function/Animals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace function
+{
+    class Animals
+    {
+        string name;
+        public Animals(string name)
+        {
+            this.name = name;    
+        }
+
+        public void NamudiHayvon(string name)
+        {
+            Console.WriteLine($"Hayvoni khonagi {name}");
+        }
+
+        public (int Age, string Name) Hayvon(int age, string name)
+        {
+            return (age, name);
+        } 
+    }
+}
---
+++ function/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals animals = new Animals("sag");
+            animals.NamudiHayvon("sag");
+            string functionText = Convert.ToString(animals.Hayvon(12, "gurba"));
+            Console.WriteLine(functionText);
+            Console.WriteLine(animals.Hayvon(12, "gurba").Name);
+            Console.ReadLine();
+        }
+    }
+}
