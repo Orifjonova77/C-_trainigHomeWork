59b5d97c87632a7ce4498f15b24df5621f96f374
orasta95 <94688630+orasta95@users.noreply.github.com>
2021-11-26 15:54:14 +0500

    Add files via upload
    new file

---
+++ Form1(1).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Second_Home_Work
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int weeks = int.Parse(textBox1.Text);
+            int months = int.Parse(textBox2.Text);
+            int years = int.Parse(textBox3.Text);
+
+            label4.Text = "Количество дней: " + ((weeks * 7) + (months * 30) + (years * 360));
+        }
+    }
+}
